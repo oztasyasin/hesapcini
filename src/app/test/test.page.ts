3fad1bd21b8d6becf1c63d5344a011c2d1bc1773
oztasyasin <55958441+oztasyasin@users.noreply.github.com>
2022-07-19 14:05:51 +0300

    bug fixes

--- src/app/test/test.page.ts
+++ src/app/test/test.page.ts
@@ -9,15 +9,15 @@ import { PopoverCComponent } from '../popover-c/popover-c.component';
 export class TestPage implements OnInit {
 
   constructor(public popoverController: PopoverController) { }
- 
-  popoverData=[
+
+  popoverData = [
     {
-      title:"Düzenle",
-      icon:"pencil",
+      title: "Düzenle",
+      icon: "pencil",
     },
     {
-      title:"Sil",
-      icon:"trash",
+      title: "Sil",
+      icon: "trash",
     },
     {
       title: "İptal Et",
@@ -40,33 +40,53 @@ export class TestPage implements OnInit {
   bottom = 0;
   bottomStr = "0%";
   ngOnInit(): void {
-    
+
   }
-  async buttonClick(event:any){
+  async buttonClick(event: any) {
     const popover = await this.popoverController.create({
       component: PopoverCComponent,
       cssClass: 'my-custom-class',
-      componentProps: {data: "dsad"},
+      componentProps: { data: "dsad" },
       event: event,
       translucent: true
     });
     await popover.present();
-  
+
     const { role } = await popover.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
   }
-  options(){
-    this.state=!this.state;
+  options() {
+    this.state = !this.state;
   }
-  mainFrameClick(){
+  mainFrameClick() {
     console.log("main frame");
-    
+
   }
-  iconClick(){
+  iconClick() {
     console.log("icon");
   }
-  bottomValue(){
+  bottomValue() {
     this.bottom = this.bottom + 10
     this.bottomStr = this.bottom + "%";
+  }
+  x = '150px';
+  y = '0px';
+  getCoordinates(e) {
+    let tX = 0;
+    let tY = 0;
+    tX = e.clientX;
+    tY = e.clientY;
+    this.x = tX + 'px';
+    console.log(tX + ',' + tY);
+    this.getY(tX)
+  }
+  getY(x){
+    let r = 140;
+    x = r-x; 
+    let y2 = (r*r)-(x*x);
+    y2 = Math.sqrt(y2);
+    console.log(y2);
+    this.y = y2+'px';
+
   }
 }
